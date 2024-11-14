9617d154b68a6d2c541da09fe7500d2db35af608
albertoOlmo1223 <albertoolmofp@ibf.cat>
2024-11-14 17:58:25 +0100

    Proyecto 1

--- ModeloVistaControlador/views/Inicio.php
+++ ModeloVistaControlador/views/Inicio.php
@@ -1,39 +1,51 @@
 <!-- Primera seccion: Bienvenida -->
 <section id="fondo-primera-seccion">
-    <div id="seccion-bienvenida">
-        <h1 class="h1-small">¡Bienvenido!</h1>
-        <h1>¿Tienes hambre?</h1>
-        <p class="h1-p">Tu hambre, nuestra especialidad. Explora los sabores únicos que tenemos preparados para ti y
-            disfruta de una experiencia gastronómica sin salir del trabajo!</p>
-        <div id="efecto-pressDown">
-            <a href="?controller=producto&action=menu" class="primaryButton-yellow-2">Pide ya</a>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col col-md-10 col-lg-8 mx-auto">
+                <div id="seccion-bienvenida" class="">
+                    <h1 class="h1-small">¡Bienvenido!</h1>
+                    <h1>¿Tienes hambre?</h1>
+                    <p class="h1-p">Tu hambre, nuestra especialidad. Explora los sabores únicos que tenemos preparados para ti y
+                    disfruta de una experiencia gastronómica sin salir del trabajo!</p>
+                    <div id="efecto-pressDown">
+                        <a href="?controller=producto&action=menu" class="primaryButton-yellow-2">Pide ya</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
 
 <section id="seccion-ofertas">
-    <div id="ofertas">
-        <h2 class="h2-fondoNegro">Ofertas</h2>
-        <div id="carousel-container">
-            <button id="prev-btn" class="carousel-btn">&#10094;</button>
-            <div id="lista-ofertas">
-                <?php foreach ($ofertas as $index => $oferta) { ?>
-                    <div class="estilo-oferta">
-                        <div class="imagenes-compuestas">
-                            <div class="img-oferta">
-                                <img src="<?= $oferta->getImagen(); ?>" class="card-img-top" alt="oferta<?= $oferta->getID(); ?>-productos">
-                            </div>
-                            <div class="img-icono-oferta">
-                                <img src="imagenes/Iconos/Descuento.png" alt="Oferta">
-                            </div>
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            <div class="col col-md-10 col-lg-8 mx-auto">
+                <div id="ofertas">
+                    <h2 class="h2-fondoNegro">Ofertas</h2>
+                    <div id="carousel-container">
+                        <button id="prev-btn" class="carousel-btn">&#10094;</button>
+                        <div id="lista-ofertas">
+                            <?php foreach ($ofertas as $index => $oferta) { ?>
+                                <div class="estilo-oferta">
+                                    <div class="imagenes-compuestas">
+                                        <div class="img-oferta">
+                                            <img src="<?= $oferta->getImagen(); ?>" class="card-img-top" alt="oferta<?= $oferta->getID(); ?>-productos">
+                                        </div>
+                                        <div class="img-icono-oferta">
+                                            <img src="imagenes/Iconos/Descuento.png" alt="Oferta">
+                                        </div>
+                                    </div>
+                                    <p class="h3-p-fondoNegro-variante-1 separacion-oferta"><?= $oferta->getCategoria(); ?></p>
+                                    <h3 class="h3-fondoNegro"><?= $oferta->getNombre(); ?></h3>
+                                    <p class="h3-p-fondoNegro"><?= $oferta->getDescripcion(); ?></p>
+                                </div>
+                            <?php } ?>
                         </div>
-                        <p class="h3-p-fondoNegro-variante-1 separacion-oferta"><?= $oferta->getCategoria(); ?></p>
-                        <h3 class="h3-fondoNegro"><?= $oferta->getNombre(); ?></h3>
-                        <p class="h3-p-fondoNegro"><?= $oferta->getDescripcion(); ?></p>
+                        <button id="next-btn" class="carousel-btn">&#10095;</button>
                     </div>
-                <?php } ?>
+                </div>
             </div>
-            <button id="next-btn" class="carousel-btn">&#10095;</button>
         </div>
     </div>
 </section>
@@ -68,24 +80,36 @@
 
 <!-- Seccion: Sobre nosotros -->
 <section id="seccion-sobreNosotros">
-    <div id="contenido-sobreNosotros">
-        <h2>¡Nuestra historia!</h2>
-        <p>Desde 2010, hemos dedicado nuestra pasion a ofrecer hamburgesas que celebran la frescura y los sabores auténticos. Cada plato cuenta una historia y está hecho con los mejores ingredientes...</p>
-        <a href="views/Sobre nosotros.php" class="neutralButton-white">Leer mas</a>
-        <img src="imagenes/Fondos/imagen-sobreNosotros.png" alt="">
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            <div class="col col-md-10 col-lg-8 mx-auto">
+                <div id="contenido-sobreNosotros">
+                    <h2>¡Nuestra historia!</h2>
+                    <p>Desde 2010, hemos dedicado nuestra pasion a ofrecer hamburgesas que celebran la frescura y los sabores auténticos. Cada plato cuenta una historia y está hecho con los mejores ingredientes...</p>
+                    <a href="views/Sobre nosotros.php" class="neutralButton-white">Leer mas</a>
+                    <img src="imagenes/Fondos/imagen-sobreNosotros.png" alt="">
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
 <!-- Seccion: Contacto -->
 <section id="seccion-contacto">
-    <div id="contenido-contacto">
-        <div class="contenido-texto">
-            <h2 class="h2-fondoNegro">¿Tienes alguna duda?</h2>
-            <p class="h2-p-fondoNegro">We think Super is the best way to publish content online which is why we use it for all our sites. The content for this site (the one you are reading now), is all coming from Notion.</p>
-            <a href="views/Contacto.php" class="neutralButton-white">Contactanos!</a>
-        </div>
-        <div class="contenido-imagen">
-            <img src="imagenes/Fondos/imagen-contacto.png" alt="">
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            <div class="col col-md-10 col-lg-8 mx-auto">
+                <div id="contenido-contacto">
+                    <div class="contenido-texto">
+                        <h2 class="h2-fondoNegro">¿Tienes alguna duda?</h2>
+                        <p class="h2-p-fondoNegro">We think Super is the best way to publish content online which is why we use it for all our sites. The content for this site (the one you are reading now), is all coming from Notion.</p>
+                        <a href="views/Contacto.php" class="neutralButton-white">Contactanos!</a>
+                    </div>
+                    <div class="contenido-imagen">
+                        <img src="imagenes/Fondos/imagen-contacto.png" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
