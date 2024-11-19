e27fd493adb58a7e92b7d3d16246c398fd49e192
albertoOlmo1223 <albertoolmofp@ibf.cat>
2024-11-19 18:00:51 +0100

    Proyecto 1

--- ModeloVistaControlador/views/header/header.php
+++ ModeloVistaControlador/views/header/header.php
@@ -21,28 +21,26 @@
                     <a href="?controller=producto&action=menu" class="apartado-menu">Menu</a>
                     <a href="?controller=producto&action=sobreNosotros" class="apartado-sobreNosotros">Sobre nosotros</a>
                     <a href="?controller=producto&action=contacto" class="apartado-contacto">Contacto</a>
-                </nav>
+                    <div class="iniciarSesion d-flex align-items-center">
+                        <a href="?controller=user" class="primaryButton-yellow-1">Iniciar sesión</a>
+                    </div>  
+                </nav> 
 
-                <div class="iniciarSesion d-flex align-items-center">
-                    <a href="?controller=user" class="primaryButton-yellow-1">Iniciar sesión</a>
-                    
-
-                    <div class="menu">
-                        <div class="dropdown">
-                        <button class="dropdown-btn" aria-label="Abrir menú">
-                            <img src="imagenes/Iconos/menu-24.svg" class="icono-menu" alt="Menú">
-                            <img src="imagenes/Iconos/close-24.svg" class="icono-close" alt="Close">
-                        </button>
-                            <div class="contenido">
-                                <a href="?controller=producto" class="apartado-menu">Inicio</a>
-                                <a href="?controller=producto&action=menu" class="apartado-menu">Menu</a>
-                                <a href="?controller=producto&action=sobreNosotros" class="apartado-sobreNosotros">Sobre nosotros</a>
-                                <a href="?controller=producto&action=contacto" class="apartado-contacto">Contacto</a>
-                            </div>
+                <div class="menu">
+                    <div class="dropdown">
+                    <button class="dropdown-btn" aria-label="Abrir menú">
+                        <img src="imagenes/Iconos/menu-24.svg" class="icono-menu" alt="Menú">
+                        <img src="imagenes/Iconos/close-24.svg" class="icono-close" alt="Close">
+                    </button>
+                        <div class="contenido">
+                            <a href="?controller=producto" class="apartado-menu">Inicio</a>
+                            <a href="?controller=producto&action=menu" class="apartado-menu">Menu</a>
+                            <a href="?controller=producto&action=sobreNosotros" class="apartado-sobreNosotros">Sobre nosotros</a>
+                            <a href="?controller=producto&action=contacto" class="apartado-contacto">Contacto</a>
                         </div>
                     </div>
-
                 </div>
+
             </div>
         </div>
     </div>
