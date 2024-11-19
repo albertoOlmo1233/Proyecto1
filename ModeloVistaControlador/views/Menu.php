74fa1e336ebae9cfafc44c636f0aaaa0de721851
albertoOlmo1223 <albertoolmofp@ibf.cat>
2024-11-19 17:38:55 +0100

    Proyecto 1

--- ModeloVistaControlador/views/Menu.php
+++ ModeloVistaControlador/views/Menu.php
@@ -1,8 +1,8 @@
 <?php include_once("views/header/header.php");?>
 <section id="seccion-nuestraCarta">
     <div class="container">
-        <div class="row">
-            <div class="col-12">
+        <div class="row d-flex justify-content-center">
+            <div class="col-sm-12 col-sm-9 col-lg-9">
                 <div id="contenido-descripcionCarta">
                     <h2>Nuestra carta</h2>
                     <!-- Migas de pan -->
@@ -27,26 +27,30 @@
 
 <section id="seccion-productos">
     <div class="container"> <!-- Usa container-fluid para eliminar los márgenes -->
-        <div class="row g-4"> <!-- Sin espacio entre columnas -->
-            <h3 class="titulo-carta"><?=$tituloProducto?></h3>
-            <?php
-                foreach($productos as $producto) {
-            ?>
-                <div class="col-sm-12 col-md-6 col-lg-4 mt-0"> <!-- Sin padding horizontal -->
-                    <div class="card h-100" style="width: 100%;"> 
-                        <a href="?controller=producto&action=show&id=<?=$producto->getID()?>">
-                            <img src="<?=$producto->getImagen();?>" class="card-img-top" alt="Imagen del producto">
-                        </a>
-                        <div class="card-body">
-                            <h3 class="card-title"><?=$producto->getNombre();?></h3>
-                            <p class="card-text"><?=$producto->getPrecio();?> €</p>
-                            <a href="" class="neutralButton-white">Añadir al carrito</a>
+        <div class="row g-4 d-flex justify-content-center"> <!-- Sin espacio entre columnas -->
+            <div class="col-sm-12 col-sm-9 col-lg-9">
+                <div class="row">
+                <h3 class="titulo-carta"><?=$tituloProducto?></h3>
+                    <?php
+                        foreach($productos as $producto) {
+                    ?>
+                        <div class="col-sm-12 col-md-6 col-lg-4 mt-0"> <!-- Sin padding horizontal -->
+                            <div class="card h-100" style="width: 100%;"> 
+                                <a href="?controller=producto&action=show&id=<?=$producto->getID()?>">
+                                    <img src="<?=$producto->getImagen();?>" class="card-img-top" alt="Imagen del producto">
+                                </a>
+                                <div class="card-body">
+                                    <h3 class="card-title"><?=$producto->getNombre();?></h3>
+                                    <p class="card-text"><?=$producto->getPrecio();?> €</p>
+                                    <a href="" class="neutralButton-white">Añadir al carrito</a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    <?php
+                        }
+                    ?>
                 </div>
-            <?php
-                }
-            ?>
+            </div>
         </div>
     </div>
 </section>
