e97ebbb281878766ad23b1de6da967d59afae884
albertoOlmo1223 <albertoolmofp@ibf.cat>
2024-11-20 17:58:08 +0100

    Proyecto 1

--- ModeloVistaControlador/views/Sobre nosotros.php
+++ ModeloVistaControlador/views/Sobre nosotros.php
@@ -15,19 +15,17 @@ include_once("views/header/header.php"); ?>
                     </div>
                 </div>
                 <!-- Contenido sobre nosotros -->
-                <div class="col-12">
-                    <div id="contenido-sobre-nosotros">
-                        <div class="migas-de-pan ">
-                            <!-- Migas de pan -->
-                            <a href="?controller=producto">Inicio</a>
-                            <p>-</p>
-                            <a href="?controller=producto&action=sobreNosotros">Sobre nosotros</a>
-                        </div>
-                        <h1 class="p-0">Sobre nosotros</h1>
-                        <span>
-                        Creating with Super is more than just building a website; it's about enabling you to communicate with and connect to your audience. Our automatic optimizations take care of the technicalities, so you can focus on creating engaging content, delivered at unbeatable speeds.
-                        </span>
+                <div id="contenido-sobre-nosotros col-12 p-5">
+                    <div class="migas-de-pan ">
+                        <!-- Migas de pan -->
+                        <a href="?controller=producto">Inicio</a>
+                        <p>-</p>
+                        <a href="?controller=producto&action=sobreNosotros">Sobre nosotros</a>
                     </div>
+                    <h1 class="p-0">Sobre nosotros</h1>
+                    <span class="descripcion">
+                    Creating with Super is more than just building a website; it's about enabling you to communicate with and connect to your audience. Our automatic optimizations take care of the technicalities, so you can focus on creating engaging content, delivered at unbeatable speeds.
+                    </span>
                 </div>
             </div>
         </div>
