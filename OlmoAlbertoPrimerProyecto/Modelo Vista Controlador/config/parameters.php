51592de84a7ddb9b463094a356a9285ab88b3606
albertoOlmo1233 <albertoolmofp@ibf.cat>
2025-01-07 02:07:27 +0100

    Pagina web final

---
+++ OlmoAlbertoPrimerProyecto/Modelo Vista Controlador/config/parameters.php
@@ -0,0 +1,5 @@
+<?php
+
+define("default_action", "index")
+
+?>
---
+++ OlmoAlbertoPrimerProyecto/Modelo Vista Controlador/controllers/productoController.php
@@ -0,0 +1,13 @@
+<?php 
+
+class productoController {
+    public function index() {
+        include_once 'views/productos/index.php';
+    }
+    public function show() {
+        include_once 'views/productos/show.php';
+    }
+
+}
+
+?>
